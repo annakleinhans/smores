8041fa2b3b9c5b5ad06052a752512fbc81d802f8
annakleinhans <annakleinhans@students.berkeley.net>
2019-02-13 14:02:56 -0800

    Anna finished Basic Java Class exercises

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,2 +1,39 @@
 public class Rectangle {
+
+
+
+    private double length;
+    private double width;
+
+
+    public double getWidth(){
+        return width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double blue) {
+        this.length = blue;
+    }
+
+    public void setWidth(double red) {
+        this.width = red;
+    }
+
+    public double getArea(){
+        return length * width;
+    }
+
+    public double getDiagonal(){
+       return  Math.sqrt((length * length) + (width * width));
+    }
+
+    public boolean isSquare(){
+        if (length == width){
+            return true;
+        }
+        return false;
+    }
 }
--- src/Runner.java
+++ src/Runner.java
@@ -1,2 +1,10 @@
 public class Runner {
+    public static void main(String[] args) {
+        Car moo = new Car(2000, "Tesla", 0, "Model S");
+        System.out.println(moo.toString());
+        moo.drive(90);
+        System.out.println(moo.toString());
+        Rectangle circle = new Rectangle();
+        }
 }
+
